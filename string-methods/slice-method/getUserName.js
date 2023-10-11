83d65faf1409ee1b8b36d91348f74af5aeb6d9bf
ramGit22 <karki_ram@hotmail.com>
2023-10-11 12:48:41 +0300

    refactor

---
+++ string-methods/slice-method/getUserName.js
@@ -0,0 +1,39 @@
+// function getUserName(email) {
+//   const indexOfAt = email.indexOf('@')
+//   const userName = email.slice(0, indexOfAt)
+//   return userName
+// }
+// const email = 'john.doe@example.com'
+// const userName = getUserName(email)
+// console.log(userName)
+
+// Solution using 'For'loop
+// function getUserName(email) {
+//   let userName = ''
+//   for (let i = 0; i < email.length; i++) {
+//     if (email[i] === '@') {
+//       break
+//     }
+//     userName += email[i]
+//   }
+//   return userName
+// }
+// const email = 'john.doe@example.com'
+// const userName = getUserName(email)
+// console.log(userName)
+
+// Solution using 'Array' and 'For' loop
+// function getUserName(email) {
+//   let userName = ''
+//   const emailArray = email.split('')
+//   for (let i = 0; i < emailArray.length; i++) {
+//     if (emailArray[i] === '@') {
+//       break
+//     }
+//     userName += emailArray[i]
+//   }
+//   return userName
+// }
+// const email = 'john.doe@example.com'
+// const userName = getUserName(email)
+// console.log(userName)
